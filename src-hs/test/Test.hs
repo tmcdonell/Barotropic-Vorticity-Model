80aa91164d7465e780105b682e464ed93358db34
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-02-25 12:16:12 +1100

    add empty haskell project

---
+++ src-hs/test/Test.hs
@@ -0,0 +1,12 @@
+-- |
+-- Module      : Test
+-- Copyright   : [2018] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
